c1362ea1e932e715dd23ebe861475a99659f0795
Marcelle Model <informatica@drogcidade.com.br>
2025-07-08 17:33:28 -0300

    updated download_table

--- scripts/download_table.py
+++ scripts/download_table.py
@@ -34,7 +34,7 @@ download_dir = os.getcwd()
 
 # set up chrome options for headless mode/configure download behavior
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")  
+chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920,1080")
