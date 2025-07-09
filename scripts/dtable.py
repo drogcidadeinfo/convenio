5710fe8201d4591040134fb46b693cd9fbcc13a1
Drogaria Cidade <informatica@drogcidade.com.br>
2025-07-09 09:29:09 -0300

    Update dtable.py

--- scripts/dtable.py
+++ scripts/dtable.py
@@ -40,6 +40,9 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920,1080")  # Set dimensions
+chrome_options.add_argument("--start-maximized")  # Maximize window
+chrome_options.add_argument("--force-device-scale-factor=1")  # Prevent scaling
 chrome_options.add_experimental_option("prefs", {
     "download.default_directory": DOWNLOAD_DIR,
     "plugins.always_open_pdf_externally": True,
